1681168a2ecc86b49d4f8fb336a4de5fc0d73af7
pritamshinde411-dotcom <pritamshinde411@gmail.com>
2026-02-01 15:50:48 +0530

    Create task2_calculator.cpp  ( Task 2 completed)
    Implemented a calculator program in c++ that performs addition,subtraction,multiplication and division

---
+++ task2_calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+  int main(){
+     double num1,num2;
+     char operation;
+    cout<<"Enter first number:";
+    cin>>num1;
+    cout<<"Enter second number:";
+    cin>>num2;
+   cout<<"Enter an operator(+,-,*,/)";
+   cin>> operation;
+
+      switch(operation){
+case '+': 
+      cout<<num1<<"+"<< num2<<"="<<(num1 + num2)<<endl;
+   break;
+case'-':
+     cout<<num1<<"-"<<num2<<"="<<(num1 - num2)<<endl;
+  break;
+  case'*':
+     cout<<num1<<"*"<<num2<<"="<<(num1 * num2)<<endl;
+break;
+case '/':
+       if (num2 != 0){
+ cout<<num1<<"/"<<num2<<"="<<(num1 /num2)<<endl;
+break;
+       }else{
+  cout<<"Error Division by zero!"<<endl;
+    }
+break;
+default: 
+        cout<<"Invalid operator!"<<endl;
+      }
+return 0;
+  }
+    
